2d1dbd5883a4e7ffcbda9fc92adf5198f459f13d
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-02 17:21:05 +0100

    Add files via upload

---
+++ user_model.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model) :
+
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    
+    data_files = db.relationship("Data_File", back_populates="user")
