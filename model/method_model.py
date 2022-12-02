157a047fa3871cb7d6de566535be7a31ea341f7c
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-02 15:24:29 +0100

    Delete method_model.py

--- model/method_model.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Method(db.Model) :
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    method_name = db.Column(db.String(100), unique=True, nullable=False)
-    details = db.Column(db.String(200))
-    number_of_args = db.Column(db.Integer(), nullable=False)
