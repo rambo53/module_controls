c8caa9014da70c99c4e066fa2ff585a7e35644dc
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-02 17:25:06 +0100

    Update data_file_service.py

--- controls/services/data_file_service.py
+++ controls/services/data_file_service.py
@@ -4,14 +4,17 @@ import uuid
 from app import db
 
 def get_data_files_service():
-    dict_data_files = {}
+    dict_data_files = []
     try:
         data_files = Data_File.query.all()
         for data_file in data_files:
-            dict_data_files[data_file.id]={
+            
+            dict_data_files.append({
+                'user_name': data_file.user.name,
+                'public_id': data_file.id_public,
                 'file_name':data_file.file_name,
                 'configs': data_file.configs[0].name +" "+ data_file.configs[1].name
-            }
+            })
 
         return jsonify(dict_data_files)
     except Exception as e:
