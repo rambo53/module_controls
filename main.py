0db7fd659055e1ab9e1869aa312716a25ed4a0fe
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-02 15:25:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from app import app
 from flask_cors import CORS
-from api.method_api import method_route
+from controls.api.method_api import method_route
+from controls.api.config_api import config_route
+from controls.api.data_file_api import data_file_route
+from controls.api.error_file_api import error_file_route
 
 app.register_blueprint(method_route, url_prefix='/method')
+app.register_blueprint(config_route, url_prefix='/config')
+app.register_blueprint(data_file_route, url_prefix='/data_file')
+app.register_blueprint(error_file_route, url_prefix='/error_file')
 
 CORS(app)
 
