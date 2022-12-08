a798b95719476becb0c83467cd8315c6af2ae82a
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-08 09:11:52 +0100

    Update data_file_api.py

--- controls/api/data_file_api.py
+++ controls/api/data_file_api.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, request
-from controls.services.data_file_service import get_data_files_service, add_data_files_service
+from controls.services.data_file_service import get_data_files_service, add_data_files_service, get_data_file_service
 
 data_file_route = Blueprint('data_file', __name__)
 
 @data_file_route.route("/data_files", methods=['GET'])
 def get_data_files():
     return get_data_files_service()
+
+@data_file_route.route("/data_file/<id_public>", methods=['GET'])
+def get_data_file(id_public):
+    return get_data_file_service(id_public)
 
 @data_file_route.route("/data_file", methods=['POST'])
 def add_data_files():
