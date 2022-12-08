8c2669be1b1ddbaa8c5bc5828d79553c8acee918
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-08 09:14:13 +0100

    Update method_service.py

--- controls/services/method_service.py
+++ controls/services/method_service.py
@@ -3,18 +3,23 @@ from flask import jsonify, make_response
 import uuid
 from app import db
 
+
 def get_methods_service():
-    dict_methods = {}
     try:
         methods = Method.query.all()
+        list_methods = []
         for method in methods:
-            dict_methods[method.id]={
-                'name':method.method_name,
+            dict_method={
+                'id':method.id_public,
+                'method_name':method.method_name,
+                'public_name':method.public_name,
+                'need_args':method.need_args,
             }
-
-        return jsonify(dict_methods)
+            list_methods.append(dict_method)
+        return jsonify(list_methods)
     except Exception as e:
         return make_response({'message' : str(e)}, 404)
+
 
 def get_method_service(id_public):
     try:
@@ -25,6 +30,7 @@ def get_method_service(id_public):
         return jsonify(model)
     except Exception as e:
         return jsonify({'message' : str(e), 'status': 404}) 
+
 
 def add_method_service(data):
     try:
@@ -38,6 +44,7 @@ def add_method_service(data):
         
     except Exception as e:
         return jsonify({'message' : str(e), 'status': 404}) 
+
 
 def update_method_service(id_public, data):
     try:
