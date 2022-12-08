f824c638847c4d50e7720b6f423093d7fdd2279d
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-08 09:13:47 +0100

    Update data_file_service.py

--- controls/services/data_file_service.py
+++ controls/services/data_file_service.py
@@ -1,16 +1,17 @@
 from controls.model.data_file_model import Data_File
 from controls.utils.directory_utils import create_directory, get_file_extension, get_filename
-from controls.utils.date_utils import get_datetime, format_datetime
+from controls.utils.date_utils import get_datetime
 from controls.utils.fake_data_utils import get_fake_id_user
+from controls.utils.df_utils import Df_Utils
 from flask import jsonify
 import uuid
 from app import db
+import sys
 
 def get_data_files_service():
-    dict_data_files = []
     try:
         data_files = Data_File.query.all()
-        dict_data_files = []
+        list_data_files = []
         for data_file in data_files:
 
             lst_config = []
@@ -19,7 +20,8 @@ def get_data_files_service():
 
                 for config in data_file.configs:
                     new_dict_config = {
-                        'config_name':config.name,
+                        'id':config.id_public,
+                        'name':config.name,
                         'config_user_create':config.user.name,
                     }
                     lst_config.append(new_dict_config)
@@ -31,36 +33,42 @@ def get_data_files_service():
                 'configs': lst_config
             }
 
-            dict_data_files.append(new_dict_data_file)
-            '''
-            dict_data_files[data_file.id_public]={
-                'user_name': data_file.user.name,
-                'public_id': data_file.id_public,
-                'file_name':data_file.file_name,
-            }
-            if len(data_file.configs) > 0:
-                dict_data_files[data_file.id_public]['configs'] = {}
-                for config in data_file.configs:
-                    dict_data_files[data_file.id_public]['configs'][config.id_public] = {
-                        'config_name':config.name,
-                        'config_user_create':config.user.name,
-                        #'config_create':format_datetime(config.created_at)
-                    }
-            '''
-        return jsonify(dict_data_files)
+            list_data_files.append(new_dict_data_file)
+
+        return jsonify(list_data_files)
+    except Exception as e:
+        return jsonify({'message' : str(e), "status":404})
+
+
+def get_data_file_service(id_public):
+    try:
+        data_file = Data_File.query.filter(Data_File.id_public==id_public).first_or_404()
+        model = {
+            'id': data_file.id_public,
+            'name': data_file.file_name,
+            'nb_rows': data_file.nb_rows,
+            'nb_cols':data_file.nb_cols,
+            'first_rows':data_file.first_rows,
+        }
+        return jsonify(model)
     except Exception as e:
         return jsonify({'message' : str(e), "status":404})
 
 
 def add_data_files_service(new_file):
     try:
-        create_directory(new_file)
+        data_file_path = create_directory(new_file)
+
+        df_from_data = Df_Utils(data_file_path)
 
         data_file = Data_File()
         data_file.id_public = str(uuid.uuid4())
         data_file.file_name = get_filename(new_file.filename)
         data_file.file_extension = get_file_extension(new_file.filename)
         data_file.created_at = get_datetime()
+        data_file.nb_rows = df_from_data.nb_rows
+        data_file.nb_cols = df_from_data.nb_cols
+        data_file.first_rows = df_from_data.first_rows
 
         # TODO update when loggin user ok
         data_file.id_user_create = get_fake_id_user()
