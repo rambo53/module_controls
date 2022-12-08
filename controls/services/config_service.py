0b51b1defb2b6fdb23999d33a3e8b9f87e230564
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-08 09:13:08 +0100

    Update config_service.py

--- controls/services/config_service.py
+++ controls/services/config_service.py
@@ -15,3 +15,14 @@ def get_configs_service():
         return jsonify(dict_configs)
     except Exception as e:
         return make_response({'message' : str(e)}, 404)
+
+
+def get_config_service(id_public):
+    try:
+        config = Config.query.filter(Config.id_public==id_public).first_or_404()
+        model = {
+            'name': config.name
+        }
+        return jsonify(model)
+    except Exception as e:
+        return jsonify({'message' : str(e), "status":404})
