acf0a85b766fc53e2ccc1cfc2aeabf6771e1e97a
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-08 09:11:26 +0100

    Update config_api.py

--- controls/api/config_api.py
+++ controls/api/config_api.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request
-from controls.services.config_service import get_configs_service
-
+from controls.services.config_service import get_configs_service, get_config_service
 config_route = Blueprint('config', __name__)
 
 @config_route.route("/configs", methods=['GET'])
 def get_configs():
     return get_configs_service()
+
+@config_route.route("/config/<id_public>", methods=['GET'])
+def get_config(id_public):
+    return get_config_service(id_public)
