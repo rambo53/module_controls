d9ead5bbf99a6f930c7c03bf7e6f05d41be35700
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-16 10:43:18 +0100

    Update error_file_api.py

--- controls/api/error_file_api.py
+++ controls/api/error_file_api.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, request
-from controls.services.error_file_service import get_error_files_service
+from controls.services.error_file_service import add_error_file_service, get_error_file_service
 
 error_file_route = Blueprint('error_file', __name__)
 
-@error_file_route.route("/error_files", methods=['GET'])
-def get_error_files():
-    return get_error_files_service()
+@error_file_route.route("/<name>", methods=['GET'])
+def get_error_file(name):
+    return get_error_file_service(name)
+
+@error_file_route.route("/launch_controls", methods=['POST'])
+def add_error_file():
+    data = request.get_json()
+    return add_error_file_service(data)
+
